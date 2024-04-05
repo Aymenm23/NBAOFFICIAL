6f96a33b0201ec53e1c7be5b95170845315dc08e
Edward Vargas <edthevargas@gmail.com>
2024-04-05 07:21:13 -0400

    updating everything so it shouldnt crash

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.scss'
 import SamplePage from "./pages/SamplePage/SamplePage";
 import IndexPage from "./pages/indexPage/index";
+import ErrorPage from "./pages/errorPage/error";
 
 function App() {
-
-
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<IndexPage/>} />
       <Route path="/questions" element={<SamplePage/>} />
       <Route path="/chat" element={<SamplePage/>} />
+      <Route path="*" element={<ErrorPage/>} />
     </Routes>
-      <div>Sample App</div>
     </BrowserRouter>
   )
 }
--- src/components/indexComponents/IndexComponent.jsx
+++ src/components/indexComponents/IndexComponent.jsx
@@ -1,11 +1,34 @@
+import { Link } from 'react-router-dom';
 import './IndexComponent.scss'
+import closeIcon from "../../assets/eXit.svg"
 
-const SampleComponent = () => {
+const IndexComponent = () => {
   return (
-    <section className="cont-sample-component">
-      <h1 className="cont-sample-component__h1">Index Page Component</h1>
+    <section className="popUpCard">
+      <div className='nbaBackground'></div>
+      <div className='popUpCard__greyBackground'></div>
+        <div className='popUpCard__x'> 
+        <Link to="https://www.nba.com/" className=''>
+          <img src={closeIcon} className='xIcon'/>
+        </Link>
+      </div>
+      <div className='popUpCard__content'>
+        <div className='content'>
+        <div className='content__text'>
+          <h1 className='indexTitle'>Are you ready for the experience of a lifetime?</h1>
+        </div>
+        <div className='content__button'>
+          <Link to="/questions">
+            <div className='indexButton'>
+              <p>Proceed</p>
+            </div>
+          </Link>
+        </div>
+
+        </div>
+      </div>
     </section>
   );
 };
 
-export default SampleComponent;
+export default IndexComponent;
