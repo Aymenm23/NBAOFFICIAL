1b626a968291740ff5dddad6e576a08a195ffcdc
Edward Vargas <edthevargas@gmail.com>
2024-04-04 20:43:37 -0400

    new icon asset(x) and new AfterQuestionsComponent

---
+++ src/components/AfterQuestionComponent/AfterQuestionComponent.jsx
@@ -0,0 +1,11 @@
+import './AfterQuestionComponent'
+
+const SampleComponent = () => {
+  return (
+    <section className="">
+      <h1 className="">Sample Component</h1>
+    </section>
+  );
+};
+
+export default SampleComponent;
